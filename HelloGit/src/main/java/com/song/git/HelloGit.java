6242beb495105795248f43e06a3c2b1839e5e4cc
recurme <2810193161@qq.com>
2021-04-17 22:22:56 +0800

    SSH

--- HelloGit/src/main/java/com/song/git/HelloGit.java
+++ HelloGit/src/main/java/com/song/git/HelloGit.java
@@ -9,5 +9,6 @@ public class HelloGit {
         System.out.println("分支添加");
         System.out.println("分支添加2");
         System.out.println("在线更新");
+        System.out.println("测试SSH");
     }
 }
