d9a13e834c2b5c944536dd65fb0aa7018c058b3e
recurme <2810193161@qq.com>
2021-04-17 21:41:54 +0800

    修改分支添加

--- HelloGit/src/main/java/com/song/git/HelloGit.java
+++ HelloGit/src/main/java/com/song/git/HelloGit.java
@@ -6,7 +6,7 @@ public class HelloGit {
         System.out.println("Hello Git update1");
         System.out.println("创建分支");
         System.out.println("主干添加");
-        System.out.println("分支添加1");
+        System.out.println("分支添加");
         System.out.println("分支添加2");
         System.out.println("在线更新");
     }
